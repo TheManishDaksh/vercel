b9938ea54f4df8dfaf5cdf6498aa5a516cc236ae
TheManishDaksh <140195332+TheManishDaksh@users.noreply.github.com>
2025-04-16 20:51:27 +0530

    initial setup for deployed service

---
+++ vercel-deployFile/src/index.ts
@@ -0,0 +1,14 @@
+import { commandOptions, createClient } from "redis";
+const subscriber = createClient()
+subscriber.connect();
+
+export async function main (){
+    while(1){
+        const res = await subscriber.brPop( 
+            commandOptions({isolated:true}),
+            "upload-queue",
+            0
+        )
+    }
+    
+}
